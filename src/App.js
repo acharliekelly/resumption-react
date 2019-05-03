17d2debb6fc034c68622e81309b252d9d8f94622
Charlie <charliekelly31415@gmail.com>
2019-05-03 16:43:41 -0400

    Fix menu so routes work

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react'
 import './App.scss'
 import { Route } from 'react-router-dom'
-
-import Alert from 'react-bootstrap/Alert'
-
+import { Container, Row, Col } from 'react-bootstrap'
 import AuthenticatedRoute from './auth/components/AuthenticatedRoute'
 import Header from './header/Header'
 import Menu from './menu/Menu'
+import TimedAlert from './alert/TimedAlert'
 import SignUp from './auth/components/SignUp'
 import SignIn from './auth/components/SignIn'
 import SignOut from './auth/components/SignOut'
@@ -38,43 +37,48 @@ class App extends Component {
     const { alerts, user } = this.state
 
     return (
-      <React.Fragment>
+      <Container>
         <Header user={user} />
-        {alerts.map((alert, index) => (
-          <Alert key={index} dismissible variant={alert.type} className="userMessage">
-            <Alert.Heading>
-              {alert.message}
-            </Alert.Heading>
-          </Alert>
-        ))}
-        <main className="container">
-          <Menu user={user} />
-          <Route path='/sign-up' render={() => (
-            <SignUp alert={this.alert} setUser={this.setUser} />
-          )} />
-          <Route path='/sign-in' render={() => (
-            <SignIn alert={this.alert} setUser={this.setUser} />
-          )} />
-          <AuthenticatedRoute user={user} path='/sign-out' render={() => (
-            <SignOut alert={this.alert} clearUser={this.clearUser} user={user} />
-          )} />
-          <AuthenticatedRoute user={user} path='/change-password' render={() => (
-            <ChangePassword alert={this.alert} user={user} />
-          )} />
-          <AuthenticatedRoute user={user} exact path='/resumes' render={() => (
-            <ResumeList alert={this.alert} user={user} />
-          )} />
-          <AuthenticatedRoute user={user} path='/new' render={() => (
-            <CreateResume alert={this.alert} user={user} />
-          )} />
-          <AuthenticatedRoute user={user} exact path='/resumes/:id' render={() => (
-            <ResumeView alert={this.alert} user={user} />
-          )} />
-          <AuthenticatedRoute user={user} path='/resumes/:id/edit' render={() => (
-            <EditResume alert={this.alert} user={user} />
-          )} />
-        </main>
-      </React.Fragment>
+        <Row>
+          <Col className="col-4">
+            <Menu user={user} />
+          </Col>
+          <Col className="col-8">
+            <Route path='/sign-up' render={() => (
+              <SignUp alert={this.alert} setUser={this.setUser} />
+            )} />
+            <Route path='/sign-in' render={() => (
+              <SignIn alert={this.alert} setUser={this.setUser} />
+            )} />
+            <AuthenticatedRoute user={user} path='/sign-out' render={() => (
+              <SignOut alert={this.alert} clearUser={this.clearUser} user={user} />
+            )} />
+            <AuthenticatedRoute user={user} path='/change-password' render={() => (
+              <ChangePassword alert={this.alert} user={user} />
+            )} />
+            <AuthenticatedRoute user={user} exact path='/resumes' render={() => (
+              <ResumeList alert={this.alert} user={user} />
+            )} />
+            <AuthenticatedRoute user={user} path='/new-resume' render={() => (
+              <CreateResume alert={this.alert} user={user} />
+            )} />
+            <AuthenticatedRoute user={user} exact path='/resumes/:id' render={() => (
+              <ResumeView alert={this.alert} user={user} />
+            )} />
+            <AuthenticatedRoute user={user} path='/resumes/:id/edit' render={() => (
+              <EditResume alert={this.alert} user={user} />
+            )} />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="userMessage col-4">
+            {alerts.map((alert, index) => (
+              <TimedAlert key={index} timeout="2000" dismissible
+                variant={alert.type} message={alert.message} />
+            ))}
+          </Col>
+        </Row>
+      </Container>
     )
   }
 }
--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -1,17 +1,20 @@
 import React from 'react'
 import './menu.scss'
-
+import { NavLink } from 'react-router-dom'
 import Nav from 'react-bootstrap/Nav'
 
 const authenticated = (
-  <Nav className="flex-column" variant="pills">
-    <Nav.Item>
-      <Nav.Link href="/resumes">My Resumes</Nav.Link>
-    </Nav.Item>
-    <Nav.Item>
-      <Nav.Link href="/new">New Resume</Nav.Link>
-    </Nav.Item>
-  </Nav>
+  <div className="panel menuPanel">
+    <header>Menu</header>
+    <Nav className="flex-column" variant="pills">
+      <Nav.Item>
+        <NavLink to="/resumes" exact activeClassName="selected">My Resumes</NavLink>
+      </Nav.Item>
+      <Nav.Item>
+        <NavLink to="/new-resume" exact activeClassName="selected">New Resume</NavLink>
+      </Nav.Item>
+    </Nav>
+  </div>
 )
 
 const unauthenticated = ''
